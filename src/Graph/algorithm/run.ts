51afcf49a949bb5d2894cc8e8cdde9631aafb5e0
s2591023 <d.t.mcgrath@sms.ed.ac.uk>
2024-06-22 20:45:01 +0100

    added axios to the package.json and uncommented branzei-nisan in run.ts.

--- src/Graph/algorithm/run.ts
+++ src/Graph/algorithm/run.ts
@@ -31,9 +31,9 @@ export const runDivisionAlgorithm = async (
     case 'selfridgeConway':
       result = selfridgeConway(preferences, cakeSize)
       break
-    //case 'branzeiNisan':
-    //  result = branzeiNisan(preferences, cakeSize)
-    //  break
+    case 'branzeiNisan':
+      result = await branzeiNisan(preferences, cakeSize)
+      break
     // NOTE FOR FUTURE DEVS
     // If server-side methods are implemented,
     // be sure to `await` the asynchronous response as shown here:
