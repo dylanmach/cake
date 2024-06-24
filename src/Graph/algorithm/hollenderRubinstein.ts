4d0b081632ecc76cb3d109d5c128f6d316ff57d1
s2591023 <d.t.mcgrath@sms.ed.ac.uk>
2024-06-24 23:30:54 +0100

    Created hollenderRubinstein.ts and changed BranzeiNisan.ts so that it can differentiate between the two algorithms in the backend.

--- src/Graph/algorithm/hollenderRubinstein.ts
+++ src/Graph/algorithm/hollenderRubinstein.ts
@@ -10,33 +10,13 @@ import axios from 'axios'
 // Selfridge-Conway method use a 1-based index because that's, you know, normal.
 
 
-export const branzeiNisan = async (preferences: Preferences, cakeSize: number): 
+export const hollenderRubinstein = async (preferences: Preferences, cakeSize: number): 
 Promise<{solution: AssignedSlice[]; steps: Step[]}> => {
   try {
-    const response = await axios.post('/api/three_agent', { preferences, cakeSize });
+    const response = await axios.post('/api/four_agent', { preferences, cakeSize });
     return response.data.result;
   } catch (error) {
     console.error('Error calling API:', error);
     throw error; // Handle error as needed
   }
 };
-//export const barbanelBrams = (
-  //preferences: Preferences,
-//   cakeSize: number
-// ): { solution: AssignedSlice[]; steps: Step[] } => {
-//   if (preferences.length !== 3) {
-//     throw new Error('Barbanel-Brams only works with three agents')
-//   }
-//   const steps: Step[] = []
-//   let p0BestSlice = null
-//   let p1BestSlice = null
-//   let p2BestSlice = null
-//   let slices = []
-
-  
-//   return {
-//     solution: [...results],
-//     steps,
-//   }
-// }
-
